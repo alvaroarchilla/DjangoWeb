11e86d590c7693ef2b30174b42ff48abce3a2c39
alvaroarchilla <59779101+alvaroarchilla@users.noreply.github.com>
2021-11-13 23:16:58 +0100

    Update settings.py

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/DjangoWebProject/static/'
+STATIC_URL = 'DjangoWebProject/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/

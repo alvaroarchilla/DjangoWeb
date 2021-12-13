d66b4b5665ea8b595aec07b34375d7cf81e7104b
alvaroarchilla <59779101+alvaroarchilla@users.noreply.github.com>
2021-12-13 19:22:33 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -56,7 +56,9 @@ class CloudinaryMedia(models.Model):
     updated=models.DateField(auto_now_add=True)
     #image = CloudinaryJsFileField(attrs = { 'multiple': 1 })
     relatedpost=models.ManyToManyField(Post)
-    cloudinaryfile= CloudinaryField('image',blank=True)
+    #cloudinaryfile= CloudinaryField('image',blank=True)
+    cloudinaryfile= models.ImageField(upload_to='test/',blank=True)
+
 
     class Meta:
         verbose_name='cloudinaryMedia'

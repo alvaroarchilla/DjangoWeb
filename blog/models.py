c1a3f3b3dbe3c3e94d981572aa4d515e6a934122
alvaroarchilla <59779101+alvaroarchilla@users.noreply.github.com>
2021-12-20 19:19:45 +0100

    cloudinary file upload

--- blog/models.py
+++ blog/models.py
@@ -71,6 +71,7 @@ class CloudinaryMedia(models.Model):
 
 
 class CloudinaryFile(models.Model):
+    name=models.CharField(max_length=50,unique=True)
     cloudinaryfile= CloudinaryField('Imagen',blank=True)
     created=models.DateField(auto_now_add=True)
     updated=models.DateField(auto_now_add=True)

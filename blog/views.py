e894d68c644eb7b9560824ab8e6ab501c0f71c0e
alvaroarchilla <59779101+alvaroarchilla@users.noreply.github.com>
2021-12-28 19:09:31 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ def subcategoria(request, subcategoria_id):
 
 def post(request, post_id):
     post=Post.objects.get(id=post_id)
-    subcategoria=Categoria.objects.get(post_id)
+    subcategoria=Categoria.objects.get(id=post_id)
     relacionados=Post.objects.filter(subcategorias=subcategoria)
     return render(request, "blog/post.html", {"post":post, "relacionados":relacionados, "subcategorias":subcategoria})
 
